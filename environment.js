30ae5b7e661c8735247be248bce4bd583678e0c2
Chris Harris <chopuk@gmail.com>
2023-02-28 19:11:09 +0000

    More Changes

---
+++ environment.js
@@ -0,0 +1,11 @@
+const environments = {
+    development: {
+        AMITE : '_Uk'
+    },
+    production: {
+        YAMITE : '_Uk'
+    }
+}
+
+const env = environments.development // change this line as needed and import environment.js where needed
+export default env
